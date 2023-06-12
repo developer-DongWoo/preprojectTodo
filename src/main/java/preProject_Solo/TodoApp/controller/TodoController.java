b2564d93ebbdaac22696e277fab10927bbf537e3
developer-DongWoo <ehddn8547@gmail.com>
2023-06-12 14:20:44 +0900

    first

---
+++ src/main/java/preProject_Solo/TodoApp/controller/TodoController.java
@@ -0,0 +1,13 @@
+package preProject_Solo.TodoApp.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application !";
+    }
+}
